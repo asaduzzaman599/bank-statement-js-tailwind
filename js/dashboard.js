4989bd1001dd72cc267e325e56b75a46e7702a17
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-02-10 18:08:45 +0600

    code beautify

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,26 +1,26 @@
 // login page script 
 
-document.getElementById('login-button').addEventListener('click',function(){
+document.getElementById('login-button').addEventListener('click', function () {
     const emailInput = document.getElementById('email-input');
     const passwordInput = document.getElementById('password-input');
-    if(passwordInput.value == ''){
-        passwordInput.style.border ='2px solid red'
-    }else{
-        passwordInput.style.border ='2px solid blue'
+    if (passwordInput.value == '') {
+        passwordInput.style.border = '2px solid red'
+    } else {
+        passwordInput.style.border = '2px solid blue'
     }
-    
-    if(emailInput.value == ''){
-        emailInput.style.border ='2px solid red'
-    }else{
-        emailInput.style.border ='2px solid blue'
+
+    if (emailInput.value == '') {
+        emailInput.style.border = '2px solid red'
+    } else {
+        emailInput.style.border = '2px solid blue'
     }
-    if(emailInput.value.includes('@') && emailInput.value.includes('.') && emailInput.value != '' && passwordInput.value !=''){
-        if(emailInput.value == "a@b.c" && passwordInput.value == '1234'){
-            window.location.href= 'account.html';
-        }else{
+    if (emailInput.value.includes('@') && emailInput.value.includes('.') && emailInput.value != '' && passwordInput.value != '') {
+        if (emailInput.value == "a@b.c" && passwordInput.value == '1234') {
+            window.location.href = 'account.html';
+        } else {
             document.getElementById('errorMassage').innerText = " invalid Email or password";
         }
-    }else{
-        emailInput.style.border ='2px solid red'
+    } else {
+        emailInput.style.border = '2px solid red'
     }
 })
